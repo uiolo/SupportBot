524f3841b216436f9856fdb71282f12ff1c53f61
uiolo <vladyan198@gmail.com>
2024-03-25 15:56:00 +0300

    Added test postgre file

--- supportbot/config_reader.py
+++ supportbot/config_reader.py
@@ -1,9 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import SecretStr, Json
+from pydantic import SecretStr, Json, JsonValue
+
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     admins: Json
+
+    db_host: str
+    db_user: str
+    db_password: SecretStr
+    db_name: str
+
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
 
 config = Settings()
---
+++ supportbot/testPostgres.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+from config_reader import config
+
+try:
+    connection = psycopg2.connect(
+        host=config.db_host,
+        user=config.db_user,
+        password=config.db_password.get_secret_value(),
+        database=config.db_name
+    )
+    
+    with connection.cursor() as cur:
+        version = cur.execute(
+            'SELECT version()'
+        )
+        print(f'Server version: {version}')
+
+except Exception as _ex:
+    print('[ERROR] POSTRGE ERROR: ', _ex)
+    
+finally:
+    connection.close()
